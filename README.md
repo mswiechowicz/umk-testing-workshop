2420639ab14a63b0a4ef771514d23aa0bff6caa3
Maciej Swiechowicz <maciej.swiechowicz@allegro.pl>
2023-03-14 15:33:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UMK testing workshop
---
+++ src/arithmeticFunctions.js
@@ -0,0 +1,10 @@
+function addNumbers(a,b) {
+  return a+b;
+}
+
+function subtractNumbers(a,b) {
+  return a-b;
+}
+
+export { addNumbers, subtractNumbers }
+
